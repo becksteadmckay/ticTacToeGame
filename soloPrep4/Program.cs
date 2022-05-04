ee75f2a2acb3ec9ae758bdab747aed9051f49e2a
McKay Beckstead <bec21017@byui.edu>
2022-05-04 09:18:21 -0600

    yes

---
+++ soloPrep4/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static List<string> starterBoard()
+            {
+                List<string> board = new List<string>();
+
+                for (int i = 0; i < 9; i++){
+
+                    board.Add(i.ToString());
+
+                }
+                return board;
+            }
+            Console.Write(starterBoard());
+
+              
+
+        }
+    }
+}
